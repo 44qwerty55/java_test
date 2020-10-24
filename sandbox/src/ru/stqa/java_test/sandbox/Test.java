f1b002077fe06435164f0bac9576dd7b1ea075d2
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-10-24 15:17:04 +0300

    function

--- sandbox/src/ru/stqa/java_test/sandbox/Test.java
+++ sandbox/src/ru/stqa/java_test/sandbox/Test.java
@@ -2,8 +2,27 @@ package ru.stqa.java_test.sandbox;
 
 public class Test {
 
-    public static void main(String[] args) {
-        System.out.println(" test !!!!!!!!!!!!!  testttt!!!!!!!!!!");
-    }
+  public static void main(String[] args) {
+    testtTekst("  qwerty ");
+    double l = 4;
+    double a = 7;
+    double b = 8;
 
+
+    System.out.println(" s_1 " + testDoubl(l));
+    System.out.println(" s_2 " + testDoubl(a, b));
+  }
+
+
+  public static double testDoubl(double l) {
+    return l * l;
+  }
+
+  public static double testDoubl(double a, double b) {
+    return a * b;
+  }
+
+  public static void testtTekst(String tekst) {
+    System.out.println(" test !!!!!!!!!!!!!" + tekst + " testttt!!!!!!!!!!");
+  }
 }
