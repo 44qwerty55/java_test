22d0c25694366806b3e77645aafd492ed79038f9
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-10-24 10:51:21 +0300

    format

--- sandbox/src/ru/stqa/java_test/sandbox/Test.java
+++ sandbox/src/ru/stqa/java_test/sandbox/Test.java
@@ -2,6 +2,8 @@ package ru.stqa.java_test.sandbox;
 
 public class Test {
 
-public static void main(String[] args)	{
-System.out.println(" test !!!!!!!!!!!!!  testttt!!!!!!!!!!");
-}}
+    public static void main(String[] args) {
+        System.out.println(" test !!!!!!!!!!!!!  testttt!!!!!!!!!!");
+    }
+
+}
