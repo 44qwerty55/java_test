d333d38f64276dd95303e0ffce46e73ccaed580d
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-10-27 20:16:55 +0300

    Update TestPoint.java

--- sandbox/src/test/java/test1/TestPoint.java
+++ sandbox/src/test/java/test1/TestPoint.java
@@ -8,10 +8,10 @@ public class TestPoint {
   @Test
 
   public void PointTest() {
-    Point po = new Point(1,2);
-    Point po1 = new Point(3,6);
-  Assert.assertEquals( String.format( "%.2f" , po.distanceMetod(po1)) , "4,47");
- //   Assert.assertEquals(  po.distanceMetod(po1) , 4.47213595499958);
+    Point po = new Point(1, 2);
+    Point po1 = new Point(3, 6);
+    Assert.assertEquals(String.format("%.2f", po.distanceMetod(po1)), "4,47");
+    //   Assert.assertEquals(  po.distanceMetod(po1) , 4.47213595499958);
 
   }
 }
