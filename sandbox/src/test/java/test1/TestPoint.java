ace70dc7e960d7368af6f8e8f2064dd1a2d4da1f
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-10-27 20:14:55 +0300

    + TestPoint

---
+++ sandbox/src/test/java/test1/TestPoint.java
@@ -0,0 +1,17 @@
+package test1;
+
+import main.java.pak.Point;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestPoint {
+  @Test
+
+  public void PointTest() {
+    Point po = new Point(1,2);
+    Point po1 = new Point(3,6);
+  Assert.assertEquals( String.format( "%.2f" , po.distanceMetod(po1)) , "4,47");
+ //   Assert.assertEquals(  po.distanceMetod(po1) , 4.47213595499958);
+
+  }
+}
