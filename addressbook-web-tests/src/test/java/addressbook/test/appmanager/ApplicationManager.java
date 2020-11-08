a33d3d5898e8e263d525b2b75a7a1b1a13004edb
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-11-08 17:24:39 +0300

    + browser

--- addressbook-web-tests/src/test/java/addressbook/test/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/addressbook/test/appmanager/ApplicationManager.java
@@ -3,20 +3,40 @@ package addressbook.test.appmanager;
 
 //import org.openqa.selenium.By;
 //import org.openqa.selenium.NoSuchElementException;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
+
+//import sun.plugin2.util.BrowserType;
 
 public class ApplicationManager {
   WebDriver wd;
   private SessionHelper sessionHelper;
-  private  NavigationHelper navigationHelper;
+  private NavigationHelper navigationHelper;
   private GroupeHelper groupeHelper;
   private ContactHelper contactHelper;
+  private String browser;
+
+  public ApplicationManager(String browser) {
+    this.browser = browser;
+  }
 
   public void init() {
-    wd = new FirefoxDriver();
+   // String browser = BrowserType.FIREFOX;
+    if (browser == BrowserType.FIREFOX) {
+      wd = new FirefoxDriver();
+    } else if (browser == BrowserType.IE) {
+      wd = new InternetExplorerDriver();
+    } else if (browser == BrowserType.CHROME) {
+      wd = new ChromeDriver();
+    }
+
+    // wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupeHelper = new GroupeHelper(wd);
@@ -26,7 +46,6 @@ public class ApplicationManager {
     // login
     sessionHelper.login("admin", "secret");
   }
-
 
 
   public void stop() {
@@ -50,6 +69,7 @@ public class ApplicationManager {
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
   }
+
   public ContactHelper getContactHelper() {
     return contactHelper;
   }
--- addressbook-web-tests/src/test/java/addressbook/test/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/addressbook/test/tests/TestBase.java
@@ -1,12 +1,14 @@
 package addressbook.test.tests;
 
 import addressbook.test.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+//import sun.plugin2.util.BrowserType;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
   @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
