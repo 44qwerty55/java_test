37502a45510d212869f0adee532350e0efde2e7e
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-11-09 22:56:02 +0300

    + проверка на группу при создание контакта

--- addressbook-web-tests/src/test/java/addressbook/test/tests/ContactCreationTeste.java
+++ addressbook-web-tests/src/test/java/addressbook/test/tests/ContactCreationTeste.java
@@ -2,6 +2,7 @@ package addressbook.test.tests;
 
 import addressbook.test.model.AddContact;
 import addressbook.test.model.AddContactGroupe;
+import addressbook.test.model.GropeData;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 
@@ -12,8 +13,12 @@ public class ContactCreationTeste extends TestBase {
 
   @Test
   public void contactCreationTeste() throws Exception {
+    app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupeHelper().isThereAgroupe()) {
+      app.getGroupeHelper().createGroup(new GropeData("name1", null, null));
+    }
     app.getNavigationHelper().gotoAddContactPage();
-    app.getContactHelper().addContactForm(new AddContact("test9", "test1", "test1", "test company3", "test@test.ru", "8", "July", "1990","test_mod"), true);
+    app.getContactHelper().addContactForm(new AddContact("test9", "test1", "test1", "test company3", "test@test.ru", "8", "July", "1990","name1"), true);
  //  app.getContactHelper().addContactFormGroupe(new AddContactGroupe("test_mod"));
     app.getContactHelper().submitNewContact();
     app.getContactHelper().returnToHomePage();
