a71185d28b2d81f4476d05d80b5ab0eeb77e2b76
44qwerty55 <73277467+44qwerty55@users.noreply.github.com>
2020-11-14 11:49:23 +0300

    + Collections

---
+++ sandbox/src/main/java/pak/Collections.java
@@ -0,0 +1,18 @@
+package pak;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Collections {
+
+  public static void main(String[] args){
+    String[] langs = {"Java","tear","qweww","11121"};
+
+    List<String> languages = Arrays.asList("Java","tear","qweww","11121");
+    for (String l : languages){
+      System.out.println("qwerty  " + l);
+    }
+}
+
+}
